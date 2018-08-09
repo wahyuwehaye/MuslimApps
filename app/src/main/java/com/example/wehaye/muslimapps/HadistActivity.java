a989c218e2a0183d353733517e9c5c37a20b39c7
indah dwianti <indah.dwianti@yahoo.com>
2018-08-09 12:21:16 +0700

    sudah nambah activity hadis

---
+++ app/src/main/java/com/example/wehaye/muslimapps/HadistActivity.java
@@ -0,0 +1,13 @@
+package com.example.wehaye.muslimapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HadistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hadist);
+    }
+}
